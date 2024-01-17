8b1791449573ca8e3dba2a77092de5b83c83965e
VASILIKI TSILIGIANNI <152409743+vasilik1@users.noreply.github.com>
2024-01-17 13:50:14 +0000

    Add files via upload

---
+++ finance_calculators.py
@@ -0,0 +1,49 @@
+import math
+
+# Introduction to the calculator helps the user navigate and prompts the user to choose between 'investment' or 'bond'.
+print("Welcome to your financial calculator. Are you interested in investment or a home loan?")
+print("Please enter either 'investment' or 'bond' from the menu above to proceed:")
+
+# Corrected the assignment to use '=' instead of '=='
+user_entry = input(" ")
+
+# Validate user input and perform calculations.
+if user_entry.lower() == 'investment':
+    # Investment calculation
+    principal = float(input("Enter the amount of money you are depositing: "))
+    interest_rate = float(input("Enter the interest rate (as a percentage): "))
+    years = int(input("Enter the number of years you plan on investing: "))
+    interest_type = input("Do you want 'simple' or 'compound' interest? ")
+
+    # If the user chooses simple interest calculation for their investment:
+    if interest_type.lower() == 'simple':
+        interest = (principal * (interest_rate / 100) * years)
+
+    # If user chooses compound interest calculation for their investment:
+    elif interest_type.lower() == 'compound':
+        interest = principal * math.pow((1 + (interest_rate / 100)), years) - principal
+    else:
+        print("Invalid interest type entered. (Please enter ONLY the number of interest NOT the percentage sign.)")
+        interest = 0
+
+    # Display the result of the calculation.
+    print(f"Your {interest_type} interest will be: {interest:.2f}")
+
+# Bond calculation.
+elif user_entry.lower() == 'bond':
+    current_value = float(input("Enter the current value of the property: "))
+    interest_rate = float(input("Enter the interest rate: "))
+    months = int(input("Enter the total number of months you plan to repay your loan: "))
+
+    # Monthly interest calculation.
+    i = (interest_rate / 100) / 12
+
+    # Bond repayment calculation.
+    repayment = (i * current_value) / (1 - math.pow((1 + i), - months))
+
+    # Display the result of the calculation:
+    print(f"Your monthly loan installment will be: {repayment:.2f}")
+
+else:
+    print("Invalid entry. Please enter either 'investment' or 'bond'.")
+
